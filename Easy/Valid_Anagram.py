d976aeba5185ecf1af609af7812099f7e6ac8b3f
ShrutiK02 <kavishwarshruti72@gmail.com>
2025-06-13 11:41:55 -0700

    Added solution for Valid Anagram

---
+++ Easy/Valid_Anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ds = {}
+        dt = {}
+
+        for i in s:
+            if i in ds:
+                ds[i] = ds[i] + 1
+            else:
+                ds[i] = 1
+
+        for j in t:
+            if j in dt:
+                dt[j] = dt[j] + 1
+            else:
+                dt[j] = 1
+
+        if ds == dt:
+            return True
+        
+        return False
+        
